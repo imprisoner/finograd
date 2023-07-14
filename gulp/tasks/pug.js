e8ccc3058a73f2eb10e72b416c3c28208954f7ec
imprisoner <continentalresident@yandex.ru>
2023-07-14 18:09:20 +0400

    excluded critical from build to check if vercel reacting on it

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,28 +1,24 @@
 import gulp from 'gulp'
 import pugCompiler from 'gulp-pug'
-import { stream as critical } from "critical";
+// import { stream as critical } from "critical";
 import { GLOBS } from "../config.js"
 
 const { src, dest } = gulp
 
 export default function pug(done) {
-  try {
-    src(GLOBS.PUG.SRC)
-      .pipe(pugCompiler({
-        verbose: true,
-        pretty: true,
-        basedir: "src"
-      }))
-      .pipe(
-        critical({
-          base: 'dist/',
-          inline: true,
-          css: ['dist/css/main.css'],
-        })
-      )
-      .pipe(dest(GLOBS.PUG.DEST))
-    done()
-  } catch (err) {
-    console.log(err)
-  }
+  src(GLOBS.PUG.SRC)
+    .pipe(pugCompiler({
+      verbose: true,
+      pretty: true,
+      basedir: "src"
+    }))
+    // .pipe(
+    //   critical({
+    //     base: 'dist/',
+    //     inline: true,
+    //     css: ['dist/css/main.css'],
+    //   })
+    // )
+    .pipe(dest(GLOBS.PUG.DEST))
+  done()
 }
